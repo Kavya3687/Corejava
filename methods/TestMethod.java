a9100a9d5b0ca4e79dbaa90eb892b29b38d70b0f
Kavya3687 <kavyavishwakarma.Xworkz@gmail.com>
2021-10-26 01:25:40 +1100

    methods

---
+++ methods/TestMethod.java
@@ -0,0 +1,24 @@
+class TestMethod{
+public static void main(String []args){
+String res=Method.cricket();
+System.out.println(res);
+String resu=Method.drone();
+System.out.println(resu);
+String resl=Method.camera();
+System.out.println(resl);
+String rest=Method.makeupkit();
+System.out.println(rest);
+String result=Method.cliamate();
+System.out.println(result);
+String re=Method.sun();
+System.out.println(re);
+String resul=Method.star();
+System.out.println(resul);
+String rslt=Method.dream();
+System.out.println(rslt);
+String rs=Method.agriculture();
+System.out.println(rs);
+String es=Method.pilot();
+System.out.println(es);
+}
+}
