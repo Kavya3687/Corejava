84e865d2c60bf310acd48c5325a012d7c606dcbd
Kavya3687 <kavyavishwakarma.Xworkz@gmail.com>
2021-10-26 01:25:02 +1100

    arrays

---
+++ arrays/EvenOdd.java
@@ -0,0 +1,10 @@
+class EvenOdd{
+public static void main(String []kav){
+for(int index=0;index<kav.length;index++){
+String number=kav[index];
+System.out.print(number +"\t");
+int num=Integer.parseInt(number);
+System.out.println(num);
+}
+}
+}
